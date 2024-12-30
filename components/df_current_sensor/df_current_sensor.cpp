318521bdf24c827f1422e6bd8b8239e3cc75882e
Kyle <101596447+KyleTeal@users.noreply.github.com>
2024-12-30 13:35:50 -0600

    Update df_current_sensor.cpp

--- components/df_current_sensor/df_current_sensor.cpp
+++ components/df_current_sensor/df_current_sensor.cpp
@@ -3,7 +3,22 @@
 namespace esphome {
 namespace df_current_sensor {
 
-// Implementation is complete in the header file.
+void DFCurrentSensor::setup() {
+  ESP_LOGCONFIG("DFCurrentSensor", "Setting up DF Current Sensor...");
+}
+
+void DFCurrentSensor::update() {
+  uint8_t buffer[2];
+  if (!this->read_bytes(buffer, 2)) {
+    ESP_LOGE("DFCurrentSensor", "Failed to read from sensor!");
+    return;
+  }
+
+  uint16_t raw_value = (buffer[0] << 8) | buffer[1];
+  float current_mA = (raw_value / 65535.0) * 16.0 + 4.0;
+  float gallons = (current_mA - 4.0) * (500.0 / 16.0);
+  this->sensor_->publish_state(gallons);
+}
 
 }  // namespace df_current_sensor
 }  // namespace esphome
