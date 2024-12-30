ff52649284c5b2069469c30e48aa37707b4fe379
Kyle <101596447+KyleTeal@users.noreply.github.com>
2024-12-30 13:36:13 -0600

    Create df_current_sensor_sensor.cpp

---
+++ components/df_current_sensor/df_current_sensor_sensor.cpp
@@ -0,0 +1,9 @@
+#include "df_current_sensor_sensor.h"
+
+namespace esphome {
+namespace df_current_sensor {
+
+// All functionality is implemented in the header and `df_current_sensor`.
+
+}  // namespace df_current_sensor
+}  // namespace esphome
