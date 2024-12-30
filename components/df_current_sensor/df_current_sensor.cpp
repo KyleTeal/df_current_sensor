44e5fd61e517c896289a6f04134f3e95a87f74ea
Kyle <101596447+KyleTeal@users.noreply.github.com>
2024-12-30 13:17:36 -0600

    Update df_current_sensor.cpp

--- components/df_current_sensor/df_current_sensor.cpp
+++ components/df_current_sensor/df_current_sensor.cpp
@@ -1,17 +1,24 @@
-#pragma once
-#include "esphome.h"
+#include "df_current_sensor.h"
 
 namespace esphome {
 namespace df_current_sensor {
 
-class DFCurrentSensor : public PollingComponent, public i2c::I2CDevice {
- public:
-  DFCurrentSensor() : PollingComponent(1000) {}
-  Sensor *water_level_sensor = new Sensor();
+void DFCurrentSensor::setup() {
+  ESP_LOGCONFIG("DFCurrentSensor", "Initializing DF Current Sensor...");
+}
 
-  void setup() override;
-  void update() override;
-};
+void DFCurrentSensor::update() {
+  uint8_t buffer[2];
+  if (!read_bytes(buffer, 2)) {
+    ESP_LOGE("DFCurrentSensor", "Failed to read from sensor!");
+    return;
+  }
+
+  uint16_t raw_value = (buffer[0] << 8) | buffer[1];
+  float current_mA = (raw_value / 65535.0) * 16.0 + 4.0;
+  float gallons = (current_mA - 4.0) * (500.0 / 16.0);
+  water_level_sensor->publish_state(gallons);
+}
 
 }  // namespace df_current_sensor
 }  // namespace esphome
