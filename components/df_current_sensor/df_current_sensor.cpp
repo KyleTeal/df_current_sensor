ce4cad28e901b8d0cfe33ed7f5f285a76c38a34b
Kyle <101596447+KyleTeal@users.noreply.github.com>
2024-12-30 13:29:10 -0600

    Update df_current_sensor.cpp

--- components/df_current_sensor/df_current_sensor.cpp
+++ components/df_current_sensor/df_current_sensor.cpp
@@ -4,7 +4,7 @@ namespace esphome {
 namespace df_current_sensor {
 
 void DFCurrentSensor::setup() {
-  ESP_LOGCONFIG("DFCurrentSensor", "Initializing DF Current Sensor...");
+  ESP_LOGCONFIG("DFCurrentSensor", "Setting up DF Current Sensor...");
 }
 
 void DFCurrentSensor::update() {
