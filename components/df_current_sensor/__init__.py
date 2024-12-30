9202cdc64f6074eb2d73b80923f3d8937e311d53
Kyle <101596447+KyleTeal@users.noreply.github.com>
2024-12-30 13:37:39 -0600

    Update __init__.py

--- components/df_current_sensor/__init__.py
+++ components/df_current_sensor/__init__.py
@@ -1,1 +1,10 @@
+from esphome.components.sensor import Sensor
+from esphome.core import CORE
 
+DEPENDENCIES = ["i2c"]
+
+def setup_df_current_sensor(config):
+    if CORE.has_component("i2c"):
+        from .df_current_sensor_sensor import DFCurrentSensorSensor
+        return DFCurrentSensorSensor()
+    raise ValueError("I2C component is required for DF Current Sensor")
