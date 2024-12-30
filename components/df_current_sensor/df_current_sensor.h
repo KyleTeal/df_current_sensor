654d1cd3888d94578b8bc55e727eefe85984ade3
Kyle <101596447+KyleTeal@users.noreply.github.com>
2024-12-30 13:35:42 -0600

    Update df_current_sensor.h

--- components/df_current_sensor/df_current_sensor.h
+++ components/df_current_sensor/df_current_sensor.h
@@ -11,22 +11,8 @@ class DFCurrentSensor : public PollingComponent, public i2c::I2CDevice {
   DFCurrentSensor() : PollingComponent(1000) {}
   void set_sensor(sensor::Sensor *sensor) { this->sensor_ = sensor; }
 
-  void setup() override {
-    ESP_LOGCONFIG("DFCurrentSensor", "Setting up DF Current Sensor...");
-  }
-
-  void update() override {
-    uint8_t buffer[2];
-    if (!this->read_bytes(buffer, 2)) {
-      ESP_LOGE("DFCurrentSensor", "Failed to read from sensor!");
-      return;
-    }
-
-    uint16_t raw_value = (buffer[0] << 8) | buffer[1];
-    float current_mA = (raw_value / 65535.0) * 16.0 + 4.0;
-    float gallons = (current_mA - 4.0) * (500.0 / 16.0);
-    this->sensor_->publish_state(gallons);
-  }
+  void setup() override;
+  void update() override;
 
  protected:
   sensor::Sensor *sensor_;
